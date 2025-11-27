b7189611caba05a29136b5b0f63b7199d59678ee
Arihant Bhandari <arihantbhandari@Arihants-MacBook-Pro.local>
2025-11-27 13:51:53 +0530

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import "./globals.css";
-
+import Script from "next/script";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 export const metadata = {
@@ -15,6 +15,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9S314VF0L5');
+          `}
+        </Script>
+      </head>
       <body className="bg-gray-50 m-0 p-0">
         <Navbar /> {/* fixed navbar */}
         {children} {/* homepage or other pages */}
